bf627270eaea706c9c88958c14e0c9b5f7f4784c
Wan-Derer <frd-ru@yandex.ru>
2022-05-26 19:34:57 +0300

    Create The Simplest REST App

---
+++ src/main/java/letscode/sarafan/SarafanApplication.java
@@ -0,0 +1,17 @@
+/*
+https://www.youtube.com/watch?v=LOmcsf5IylI&list=PLU2ftbIeotGpAYRP9Iv2KLIwK36-o_qYk&index=19&ab_channel=letsCode
+ */
+
+package letscode.sarafan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarafanApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SarafanApplication.class, args);
+  }
+
+}
