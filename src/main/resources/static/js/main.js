21307f6a0c38130bd40aae508ffd6e2136ca0b3b
-- <-->
2022-05-27 12:02:25 +0300

    Add Vue.js

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,30 @@
+const messageApi = Vue.resource('/message/{id}')
+
+Vue.component('message-row', {
+    props: ['message'],
+    template: '<div><i>({{ message.id }})</i> {{ message.text }}</div>'
+});
+
+Vue.component('messages-list', {
+    props: ['messages'],
+    template:
+        '<div><h3>Список сообщений</h3>' +
+        '<message-row v-for="message in messages" :key="message.id" :message="message" />' +
+        '</div>',
+    created: function () {
+        messageApi.get().then(result =>
+            result.json().then(data =>
+                data.forEach(message => this.messages.push(message))
+            )
+        )
+    }
+});
+
+const app = new Vue({
+    el: '#app',
+    template: '<messages-list :messages="messages"/>',
+    data: {
+        messages: []
+    }
+});
+
